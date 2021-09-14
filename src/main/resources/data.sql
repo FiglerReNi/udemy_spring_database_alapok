19fda9bda1a6a135bb4dd3fe1aada3aeaf26977b
figlerr <figler.renata@tmx.hu>
2021-09-14 16:16:52 +0200

    forras

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTHDAY )
+VALUES(1,  'Ranga', 'Hyderabad',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTHDAY )
+VALUES(2,  'James', 'New York',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTHDAY )
+VALUES(3,  'Pieter', 'Amsterdam',sysdate());
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table PERSON
+(
+    id integer not null,
+    name varchar(255) not null,
+    location varchar(255),
+    birthday timestamp,
+    primary key(id)
+);
