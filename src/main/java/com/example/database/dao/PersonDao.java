c5fdcb0663e59f50fffe1cf89aac7fd6d565b8a2
figlerr <figler.renata@tmx.hu>
2021-09-16 14:48:27 +0200

    forras

--- src/main/java/com/example/database/dao/PersonDao.java
+++ src/main/java/com/example/database/dao/PersonDao.java
@@ -1,7 +1,6 @@
 package com.example.database.dao;
 
 import com.example.database.model.Person;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
